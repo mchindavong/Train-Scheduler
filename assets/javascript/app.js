1b6d08a83392e18508fa1175fff9c7eebad5df7c
Mollie <molliechindavong@gmail.com>
2017-07-16 01:58:15 -0400

    updating

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+
 var config = {
     apiKey: "AIzaSyAMGYt6hycyWHNre-68qrYlGvxKjUXcagA",
     authDomain: "clicking-things-6eb1e.firebaseapp.com",
@@ -6,46 +7,71 @@ var config = {
     storageBucket: "clicking-things-6eb1e.appspot.com",
     messagingSenderId: "743416215612"
   };
-
   firebase.initializeApp(config);
 
 var database = firebase.database();
 
-var name = "";
-var destination = "";
-var trainTime = "";
-var frequency = "";
 
-$("#add-train").on("click", function() {
+$("#add-train-btn").on("click", function(event) {
+  event.preventDefault();
 
-	event.preventDefault();
 
-	name = $("#trainName").val().trim();
-	destination = $("#destination").val().trim();
-	trainTime = $("#trainTime").val().trim();
-	frequency = $("#frequency").val().trim();
+  var trainName = $("#train-name-input").val().trim();
+  var destination = $("#destination-input").val().trim();
+  var trainTime = moment($("#time-input").val().trim(), ("HH:MM").format("X"));
+  var frequency = $("#frequency-input").val().trim();
 
-	database.ref().set({
+  var newTrain = {
+    name: trainName,
+    destination: destination,
+    time: trainTime,
+    frequency: frequency
+  };
 
-		Name: name,
-		Destination: destination,
-		TrainTime: TrainTime,
-		Frequency: frequency,
-	});
 
+  database.ref().push(newTrain);
+
+  console.log(newTrain.name);
+  console.log(newTrain.destination);
+  console.log(newTrain.time);
+  console.log(newTrain.frequency);
+
+
+  $("#train-name-input").val("");
+  $("#destination-input").val("");
+  $("#time-input").val("");
+  $("#frequency-input").val("");
 
 });
 
 
-database.ref().on("value", function(snapshot) {
+database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 
-	console.log(snapshot.val());
-	console.log(snapshot.val().name);
-	console.log(snapshot.val().role);
-	console.log(snapshot.val().startDate);
-	console.log(snapshot.val().rate);
+  console.log(childSnapshot.val());
+
+  var trainName = childSnapshot.val().name;
+  var destination = childSnapshot.val().role;
+  var trainTime = childSnapshot.val().start;
+  var frequency = childSnapshot.val().rate;
+
+  console.log(trainName);
+  console.log(destination);
+  console.log(trainTime);
+  console.log(frequency);
 
 
+  var nextArr = moment.unix(trainTime).format("HH:MM");
+
+  var min = moment().diff(moment.unix(trainTime, "X"), "minutes");
+  console.log(min);
 
 
-})
+  var minAway = min * frequency;
+  console.log(minAway);
+
+
+  $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" +
+  trainTime + "</td><td>" + min + "</td><td>" + frequency + "</td><td>" + minAway + "</td></tr>");
+});
+
+
